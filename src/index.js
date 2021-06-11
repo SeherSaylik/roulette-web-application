9e2a297f022c3199f9ecf0c593fa34af618199e7
SeherSaylik <sehersaylik@users.noreply.github.com>
2021-06-11 23:06:33 +0300

    folders-separated

--- src/index.js
+++ src/index.js
@@ -1,35 +1,40 @@
 var betPrice=0;
+var betNumbers=[];
 
 document.querySelectorAll('.number-button').forEach(function(event){
     event.addEventListener('click',function(e){
         if(e.target.style.backgroundColor=="red"){
             e.target.style.backgroundColor="#4CAF50";
             betPrice= betPrice-1;
+            betNumbers.pop(e.target.id);
             document.getElementById("bet-price-button").value=betPrice+"$";
+
         }else{
             e.target.style.backgroundColor="red";
             betPrice= betPrice+1;
+            betNumbers.push(e.target.id);
             document.getElementById("bet-price-button").value=betPrice+"$";
+            
         }
     })
 })
 
-document.querySelectorAll('.odd-even-button').forEach(function(event){
-    event.addEventListener('click',function(e){
-        if(e.target.style.backgroundColor=="red"){
-            e.target.style.backgroundColor="#4CAF50";
-            betPrice= betPrice-1;
-            document.getElementById("bet-price-button").value=betPrice+"$";
-        }else{
-            e.target.style.backgroundColor="red";
-            betPrice= betPrice+1;
-            document.getElementById("bet-price-button").value=betPrice+"$";
-        }
-    })
-})
 
+var winStatus=false;
 document.querySelector('.bet-button').addEventListener('click',function(event){
     var result=Math.floor(Math.random() * 13);
+    //document.querySelector("#spin").style.display = 'block';
+    betNumbers.forEach(compare);
+    function compare(betNumber){
+        
+        if(parseFloat(betNumber)==result){
+            window.open('/public/result.html?result='+result+'&status=win&amount=12$','betResult','width=500,height=500');
+            winStatus=true;
+        }
+    }
+    if(winStatus==false){
+        window.open('/public/result.html?result='+result+'&status=lost&amount=','betResult','width=500,height=500');
+    }
 })
 
 
